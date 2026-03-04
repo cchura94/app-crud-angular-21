7636c5bbb243e8571b1a58511da6d76c371efe7e
Cristian <cchura.cpc@gmail.com>
2026-03-03 21:22:13 -0400

    crud productos

--- src/app/producto/producto.ts
+++ src/app/producto/producto.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ProductoService } from "./producto.service"
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-producto',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './producto.html',
   styleUrl: './producto.css',
 })
@@ -13,13 +14,15 @@ export class Producto implements OnInit{
   private fb = inject(FormBuilder);
   private productoService = inject(ProductoService);
   productos = signal<any[]>([]);
+  producto_id = signal(0);
+  mostrarDialog = signal(false)
 
   productoForm = this.fb.group({
     categoria_id: [1],
-    cod_producto: [''],
+    cod_producto: ['', [Validators.required]],
     descripcion: [''],
     imagen: [''],
-    nombre: [''],
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
     precio: [''],
     stock: [''],
   })
@@ -36,9 +39,37 @@ export class Producto implements OnInit{
   }
 
   funGuardar(){
-    this.productoService.crearProducto(this.productoForm.value).subscribe(res => {
+    console.log(this.producto_id());
+    if(this.producto_id()>0){
+      // modificar
+      alert("MODIFICANDO")
+      this.productoService.modificarProducto(this.producto_id(), this.productoForm.value).subscribe(
+        (res: any) => {
+
+          this.listar();
+        }
+      )
+    }else{
+      this.productoService.crearProducto(this.productoForm.value).subscribe(res => {
       this.listar();
     })
+    this.producto_id.set(0);
+    }
+  }
+
+  funEditar(prod: any){
+    this.producto_id.set(prod.id);
+    this.productoForm.patchValue(prod)
+  }
+
+  funEliminar(id: number){
+    if(confirm("Esta seguro de eliminar?")){
+      this.productoService.eliminarProducto(id).subscribe(
+        (res) => {
+          this.listar();
+        }
+      )
+    }
   }
 
 
